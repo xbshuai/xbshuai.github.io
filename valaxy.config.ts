a8a175a6be902ef0ba5d7ae460bf4a9c9267e654
xbshuai <277889640@qq.com>
2023-10-23 10:33:44 +0800

    test changing

--- valaxy.config.ts
+++ valaxy.config.ts
@@ -1,3 +1,11 @@
+/*
+ * @Author: xbshuai 277889640@qq.com
+ * @Date: 2023-10-22 17:36:18
+ * @LastEditors: xbshuai 277889640@qq.com
+ * @LastEditTime: 2023-10-23 10:33:16
+ * @FilePath: \Blog\valaxy.config.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import { defineValaxyConfig } from 'valaxy'
 import type { UserThemeConfig } from 'valaxy-theme-yun'
 
@@ -11,6 +19,22 @@ const safelist = [
  */
 export default defineValaxyConfig<UserThemeConfig>({
   // site config see site.config.ts
+  url: 'xbshuai.github.io',
+  lang: 'zh-CN',
+  title: '小胖PACT的博客 | PACT\'s blog',
+  subtitle: '',
+  author: {
+    name: '小胖PACT',
+    avatar: 'public/pact.jpg',
+    status:
+    {
+      emoji: '😐',
+      message: '不想上班喵'
+  }},
+  mode: 'auto',
+  lastUpdated: true,
+  favicon: '',
+  description: 'Life is struggle and like a loop',
 
   theme: 'yun',
 
@@ -39,7 +63,7 @@ export default defineValaxyConfig<UserThemeConfig>({
     ],
 
     footer: {
-      since: 2016,
+      since: 2023,
       beian: {
         enable: true,
         icp: '苏ICP备17038157号',
