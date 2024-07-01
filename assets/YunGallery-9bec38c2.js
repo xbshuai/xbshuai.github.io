1418bd8b84377a1b3bc74286e5b8607904ff1a8e
xbshuai <xbshuai@users.noreply.github.com>
2024-07-01 12:14:33 +0000

    deploy: 7c806188a136028975a6983287cfa5ded53b58aa

---
+++ assets/YunGallery-9bec38c2.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./gallery-4e077ef4.js";import"./app-6307bfe1.js";import"./YunPageHeader.vue_vue_type_script_setup_true_lang-a4042c69.js";export{o as default};
---
+++ assets/_...all_-1af6accb.js
@@ -0,0 +1,1 @@
+import{f as a,aj as s,u as n,al as r,o as c,i as p}from"./app-6307bfe1.js";import{b as o}from"./route-block-83d24a4e.js";const u=a({__name:"[...all]",setup(f){const t=s(),e=n();return r(()=>{e.path!=="/"&&e.path.endsWith("/")&&t.replace(e.path.slice(0,-1))}),(i,l)=>(c(),p("div"))}});typeof o=="function"&&o(u);export{u as default};
